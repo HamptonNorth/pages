6303410ff96efd9ec529c67699a853db655fa29f
HamptonNorth <rcollins@redmug.co.uk>
2025-12-23 14:27:52 +0000

    Bumped to 0.8.3

--- src/components/rm-footer.js
+++ src/components/rm-footer.js
@@ -43,7 +43,7 @@ export class RmFooter extends LitElement {
 
   render() {
     const year = new Date().getFullYear()
-    const versionText = 'version 0.8.2'
+    const versionText = 'version 0.8.3'
 
     return html`
       <link rel="stylesheet" href="/styles/output.css" />
